3fbdfe7930f1ee207890a1c61be84e8b79d6e8c4
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-06-25 01:00:37 -0500

    Agregando props al componente Itemlistcontainer

--- proyecto_ecommerce/src/App.js
+++ proyecto_ecommerce/src/App.js
@@ -1,12 +1,16 @@
 import NavBar from "./componentes/NavBar/NavBar";
-
+import ItemListContainer from "./componentes/ItemListContainer/ItemListContainer";
 
 
 
 function App() {
+
+  const greeting = "Bienvenido a Wonder Bike";
+
   return (
     <div className="App">
       <NavBar/>
+      <ItemListContainer greeting={greeting}/>
     </div>
   );
 }
--- proyecto_ecommerce/src/componentes/CartWidget/CartWidget.js
+++ proyecto_ecommerce/src/componentes/CartWidget/CartWidget.js
@@ -2,17 +2,15 @@ import carrito from "./assets/carrito.png";
 
 const CartWindget = () => {
   return (
-
-
-
-          <div class="navbar-carrito">
-          <ul>
-            <li class="navbar-shopping-cart">
-            <img src={carrito} alt="carrito de compras" />
-            </li>
-          </ul>
-        </div>
+    <div class="navbar-carrito">
+      <ul>
+        <li class="navbar-shopping-cart">
+          <img class="navbar-shopping-cart" src={carrito} alt="carrito de compras" />
+          0
+        </li>
+      </ul>
+    </div>
   );
 };
 
-export default CartWindget
+export default CartWindget;
