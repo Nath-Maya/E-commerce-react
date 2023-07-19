c69775aec3b1b5d5416c83566189f2b143617b43
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-07-18 21:43:43 -0500

    categoryId de los elementos

--- proyecto_ecommerce/src/componentes/ItemListContainer/ItemListContainer.jsx
+++ proyecto_ecommerce/src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -2,24 +2,26 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import { getProducts } from '../../asyncMock';
 import ItemList from '../ItemList/ItemList';
-
+import { useParams } from 'react-router-dom'
 
 
 function ItemListContainer({greeting}) {
-
+   //Estado donde se almacenaran los productos obtenidos de AsyncMock
    const  [productos,setProductos] = useState([])
-
-   
-
+   //Obtener el parametro categoryId de la URL 
+   const { categoryId } = useParams()
+   // Cargar los productos al montar el componente o cuando cambia categoryId
    useEffect(() => {
-      getProducts()
+      const asyncFunc = categoryId ? getProductsByCategory : getProducts
+
+      asyncFunc(categoryId)
          .then(response => {
             setProductos(response)
-         });
-         // .catch(error => {
-         //    console.error(error);
-         // })
-   },[])
+         })
+         .catch(error => {
+             console.error(error);
+         })
+   },[categoryId])
 
    return (
       <div>
