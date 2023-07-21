b1379846f9d65e6e63fcce8e7a105816bfc4cab9
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-07-20 19:09:49 -0500

    Contador en los detalles

--- proyecto_ecommerce/src/componentes/ItemCount/ItemCount.jsx
+++ proyecto_ecommerce/src/componentes/ItemCount/ItemCount.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 //Estado donde se almacenara la cantidad del contador
 const ItemCount = ({stock, initial, onAdd}) => {
@@ -29,12 +30,15 @@ const ItemCount = ({stock, initial, onAdd}) => {
    //! Elemento que se retornara
 
    return (
-
+   <div>
+   <ListGroup.Item>Cantidad: {quantity}</ListGroup.Item>
    <ButtonGroup aria-label="Basic example">
       <Button variant="secondary" onClick={decrement}> - </Button>
       <Button variant="secondary" onClick={addCar} disabled={!stock}>Añadir al Carrito</Button>
       <Button variant="secondary" onClick={increment}> + </Button>
     </ButtonGroup>
+   </div>
+
    );
 
 };
