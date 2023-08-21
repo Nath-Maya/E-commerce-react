b0ea0c7c74725a5de87b9f2faae23dd93d0fb6d7
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-08-21 16:06:34 -0500

    Readme mejorado

--- proyecto_ecommerce/src/App.js
+++ proyecto_ecommerce/src/App.js
@@ -1,37 +1,31 @@
-import React from 'react';
+import React from "react";
 import NavBar from "./componentes/NavBar/NavBar";
-import ItemListContainer from './componentes/ItemListContainer/ItemListContainer'
-import ItemDetailContainer from './componentes/ItemDetailContainer/ItemDetailContainer'
-import Cart from './componentes/Cart/Cart';
-import Checkout from './componentes/Checkout/Checkout';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import { CarritoCompras } from './componentes/context/CartContext';
-
-
-
-
-
-
+import ItemListContainer from "./componentes/ItemListContainer/ItemListContainer";
+import ItemDetailContainer from "./componentes/ItemDetailContainer/ItemDetailContainer";
+import Cart from "./componentes/Cart/Cart";
+import Checkout from "./componentes/Checkout/Checkout";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CarritoCompras } from "./componentes/context/CartContext";
 
 function App() {
-
   return (
-
-      <BrowserRouter>
-        <CarritoCompras>
-          <NavBar />
-            <Routes>
-              <Route path='/' element={ <ItemListContainer greeting="Bienvenido a Wonder Bike"/>}/>
-              <Route path="/categoria/:categoria" element={<ItemListContainer/>} />
-              <Route path='/item/:itemId' element={<ItemDetailContainer/>}/>
-              <Route path='/cart' element={<Cart />} />
-              <Route path='/checkout' element={<Checkout/>}/>
-              <Route path='*' element={<h1>404 NOT FOUND ERROR</h1>}/>
-            </Routes>
-        </CarritoCompras>
-      </BrowserRouter>
-
+    <BrowserRouter>
+      <CarritoCompras>
+        <NavBar />
+        <Routes>
+          <Route
+            path="/"
+            element={<ItemListContainer greeting="Bienvenido a Wonder Bike" />}
+          />
+          <Route path="/categoria/:categoria" element={<ItemListContainer />} />
+          <Route path="/item/:itemId" element={<ItemDetailContainer />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="*" element={<h1>404 NOT FOUND ERROR</h1>} />
+        </Routes>
+      </CarritoCompras>
+    </BrowserRouter>
   );
 }
 
