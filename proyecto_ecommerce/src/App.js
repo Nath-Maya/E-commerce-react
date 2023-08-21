ea966678f84131afca59b3f98b5a716de8577d69
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-08-20 20:16:15 -0500

    Pagina checkout funcionando

--- proyecto_ecommerce/src/App.js
+++ proyecto_ecommerce/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import NavBar from "./componentes/NavBar/NavBar";
 import ItemListContainer from './componentes/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './componentes/ItemDetailContainer/ItemDetailContainer'
+import Cart from './componentes/Cart/Cart';
+import Checkout from './componentes/Checkout/Checkout';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import { CarritoCompras } from './componentes/context/CartContext';
-import Cart from './componentes/Cart/Cart';
+
 
 
 
@@ -24,6 +26,7 @@ function App() {
               <Route path="/categoria/:categoria" element={<ItemListContainer/>} />
               <Route path='/item/:itemId' element={<ItemDetailContainer/>}/>
               <Route path='/cart' element={<Cart />} />
+              <Route path='/checkout' element={<Checkout/>}/>
               <Route path='*' element={<h1>404 NOT FOUND ERROR</h1>}/>
             </Routes>
         </CarritoCompras>
---
+++ proyecto_ecommerce/src/componentes/Checkout/Checkout.jsx
@@ -0,0 +1,91 @@
+import { getDocs, addDoc ,collection, query, where, Timestamp, writeBatch, documentId } from "firebase/firestore";
+import { db } from '../../firebase/config'
+import {  useContext, useState } from "react";
+import CheckoutForm from "../CheckoutForm/CheckoutForm";
+import { CartContext } from "../context/CartContext";
+
+const Checkout = () => {
+  const [cargando, setCargando] = useState(false);
+  const [ordenId, setOrdenId] = useState("");
+
+  const { cart, totalQuantity, clearCart } = useContext(CartContext);
+
+  const crearOrden = async ({ name, phone, email }) => {
+    setCargando(true);
+
+    try {
+      const objOrden = {
+        buyer: {
+          name,
+          phone,
+          email,
+        },
+        items: cart,
+        total: totalQuantity(),
+        date: Timestamp.fromDate(new Date()),
+      };
+
+      const batch = writeBatch(db);
+
+      const agotadoStock = [];
+
+      const ids = cart.map((prod) => prod.id);
+
+      const productsRef = collection(db, "productos");
+
+      const productosAdicionarFirestore = await getDocs(
+        query(productsRef, where(documentId(), "in", ids))
+      );
+
+      const { docs } = productosAdicionarFirestore;
+
+      docs.forEach((doc) => {
+        const dataDoc = doc.data();
+        const stockDb = dataDoc.stock
+
+        const productoAgregadoCarrito = cart.find((prod) => prod.id === doc.id);
+        const prodQuantity = productoAgregadoCarrito?.quantity;
+
+        if (stockDb >= prodQuantity) {
+          batch.update(doc.ref, { stock: stockDb - prodQuantity });
+        } else {
+          agotadoStock.push({ id: doc.id, ...dataDoc});
+        }
+      });
+
+      if (agotadoStock.length === 0) {
+        await batch.commit();
+
+        const ordenRef = collection(db, "ordenes");
+
+        const ordenAdd = await addDoc(ordenRef, objOrden);
+
+        setOrdenId(ordenAdd.id);
+        clearCart();
+      } else {
+        console.error("Hay productos que estan fuera de stock");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setCargando(false);
+    }
+   }
+    if (cargando) {
+      return <h1>Se esta generando su orden...</h1>;
+    }
+
+    if (ordenId) {
+      return <h1>El id de su orden es: {ordenId} </h1>;
+    }
+
+    return (
+      <div>
+        <h1>Checkout</h1>
+        <CheckoutForm onConfirm={crearOrden} />
+      </div>
+    );
+
+};
+
+export default Checkout;
