1927db132746a0e8ff7cf87ad08929d0cbe081e8
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-08-22 16:16:39 -0500

    Ajustando navbar responsive

--- proyecto_ecommerce/src/componentes/NavBar/NavBar.jsx
+++ proyecto_ecommerce/src/componentes/NavBar/NavBar.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import CartWidget from "../CartWidget/CartWidget";
 import LogoHeader from "../LogoHeader/LogoHeader";
 import { NavLink, Link } from "react-router-dom";
-import Nav from 'react-bootstrap/Nav';
 import './NavBar.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
 
 const NavBar = () => {
 
   return (
-    <Nav className="header" variant="underline" defaultActiveKey="/home">
+    /*
+    <Nav  expand="lg" className="bg-body-tertiary" variant="underline" defaultActiveKey="/home">
       <Link to={'/'} className="logo-header">
           <LogoHeader/>
       </Link>
@@ -39,7 +43,55 @@ const NavBar = () => {
       </Nav.Item>
      
     </Nav>
+    */
+
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand  className="logo-header">
+          <Link to={"/"} >
+            <LogoHeader />
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto" variant="underline">
+            <Nav.Item>
+              <Nav.Link eventKey="link-1">
+                <NavLink className="navLink" to={"/"}>
+                  Home
+                </NavLink>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-2">
+                <NavLink className="navLink" to="/categoria/Accesorios">
+                  Accesorios
+                </NavLink>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-3">
+                <NavLink className="navLink" to="/categoria/Bicicletas">
+                  Bicicletas
+                </NavLink>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-4">
+                <NavLink className="navLink" to="/categoria/Ropa">
+                  Ropa
+                </NavLink>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="item-car">
+              <CartWidget />
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
+
 };
 
 export default NavBar;
